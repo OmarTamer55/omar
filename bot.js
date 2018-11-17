a07e182a744ac00f540a050a5a0bfdab0dbcf724
OmarTamer55 <45062049+OmarTamer55@users.noreply.github.com>
2018-11-17 14:06:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,12 +13,11 @@ client.on('ready', () => {
 
 client.on('ready', async() => {
 var server = "512532251850833934"; 
-var channel = "512533887814074389";
+var channel = "513312256789053441";
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('OmarIsBest')
+    client.guilds.get(server).channels.get(channel).send('OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_OmarIsBest_')
     },305);
 })
-
 
 
 
